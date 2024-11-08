b6a7a18fe0a5c06708260794e9299b1dbfc4d868
FelipeSoft <felipedecastrolima2@gmail.com>
2024-11-08 16:40:39 -0300

    [tada] First commit.

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"awesomeProject/rest"
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func main() {
+	r := rest.InitRoutes()
+	fmt.Print("HTTP server listening on port 1818")
+	log.Fatal(http.ListenAndServe(":1818", r))
 }
--- rest/prompt/PromptHandler.go
+++ rest/prompt/PromptHandler.go
@@ -1,9 +1,29 @@
 package prompt
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type Input struct {
+	Prompt string `json:"prompt"`
+}
 
 func PromptHandler(w http.ResponseWriter, r *http.Request) {
-	response := "response from ollama"
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Print("PromptHandler Error: Cannot read JSON.")
+	}
+
+	var input Input
+	err = json.Unmarshal(body, &input)
+	if err != nil {
+		fmt.Print("PromptHandler Error: Cannot unmarshal JSON.")
+	}
+	response := fmt.Sprintf("Received prompt: %s", input.Prompt)
+
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(response))
 }
